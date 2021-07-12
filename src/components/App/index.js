54ef5e385457cbd2b8a0474ca3d66b7ff295cb04
prawesh7 <praweshgaire@gmail.com>
2021-07-12 17:21:25 -0400

    Initial push

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,33 +3,22 @@ import {
     BrowserRouter as Router,
     Route, Redirect
 } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
-// import ElevateAppBar from '../components/Navbar/index'
-import GoogleCalendar from '../googleCalendar';
-import SignIn from '../Login'; 
 import Landing from '../Home'; 
-
-import NotFound from '../Notfound/pageNotFound'
-import Sidebar from '../Sidebar/index'
-import Footer from '../Footer/footer.js'
-import Patients from '../Patients/patients.js'
+import NotFound from '../Notfound/pageNotFound';
+import Footer from '../Footer/footer.js';
+import NavBar from '../Navbar';
+import SearchResults from '../SearchResults';
 
 const App = () => (
     
     <Router>
-        
-        <Sidebar/>
-        <div style={{marginTop:-50}}>
-            <Route path={ROUTES.CALENDAR} component={GoogleCalendar} />
-            <Route path={ROUTES.LOGIN} component={SignIn} />
-            <Route path={ROUTES.LANDING} component={Landing} />
-            <Route path={ROUTES.PATIENT} component={Patients} />
-            {/* <Route path={ROUTES.SIDEBAR} component={Sidebar} /> */}
-            <Route exact path="/">
-                <Redirect to="/Landing" />
-            </Route>
-            <Footer />
-        </div>
+        <NavBar/>
+        <Route path='/landing' component={Landing} />
+        <Route path='/searchresults' component={SearchResults} />
+        <Route exact path="/">
+            <Redirect to="/Landing" />
+        </Route>
+        <Footer />
     </Router>
 );
    
--- src/constants/routes.js
+++
@@ -1,8 +0,0 @@
-export const CALENDAR = '/calendar';
-export const SIDEBAR = '/sidebar';
-export const LOGIN = '/login';
-export const LANDING = '/landing'; 
-export const PATIENT = '/patients'; 
-//export const NOTFOUND = '/';
-
-
